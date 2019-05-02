42a65a2fa3c43225d9443e3f7ca4a1063d9d99a8
Chris Fung <aergonaut@gmail.com>
2019-05-02 14:32:32 -0700

    Use mini_portile2 to download/compile libjq automatically
    This commit changes extconf.rb to use mini_portile2 to automatically
    download, configure, and compile the libjq library. Previously the user
    was required to download and compile libjq separately.
    
    extconf.rb uses the --disable-maintainer-mode flag to avoid requiring an
    updated version of bison on the host.
    
    The user is still able to use their own version of libjq by passing the
    --use-system-libraries flag when installing, or by setting the
    RUBYJQ_USE_SYSTEM_LIBRARIES env var.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,33 @@
 require 'mkmf'
 
-if have_library('jq')
-  create_makefile('jq_core')
+def using_system_libraries?
+  arg_config('--use-system-libraries', !!ENV['RUBYJQ_USE_SYSTEM_LIBRARIES'])
 end
+
+unless using_system_libraries?
+  message "Buildling jq using packaged libraries.\n"
+
+  require 'rubygems'
+  require 'mini_portile2'
+
+  recipe = MiniPortile.new("jq", "1.6")
+  recipe.files = ["https://github.com/stedolan/jq/archive/jq-1.6.tar.gz"]
+  recipe.configure_options = [
+    "--enable-shared",
+    "--disable-maintainer-mode"
+  ]
+  class << recipe
+    def configure
+      execute("autoreconf", "autoreconf -i")
+      super
+    end
+  end
+  recipe.cook
+  recipe.activate
+  $LIBPATH = ["#{recipe.path}/lib"] | $LIBPATH
+  $CPPFLAGS << " -I#{recipe.path}/include"
+end
+
+abort "libjq not found" unless have_library('jq')
+
+create_makefile('jq_core')
--- ruby-jq.gemspec
+++ ruby-jq.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "multi_json"
+  spec.add_runtime_dependency "mini_portile2", ">= 2.2.0"
+
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rake-compiler"
